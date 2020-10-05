2b0e7a62bd9f38f310ef10bf23e849b1813cf3d8
ajit <ajitkrsingh0786@gmail.com>
2020-10-05 17:15:26 +0530

    5th commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React ,{Component} from 'react';
 import './App.css';
+import Axios from 'axios';
 
-function App() {
+class  App extends Component {
+       
+      constructor(props){
+        super(props);
+        this.state = {
+          users: [],
+          
+      }
+      }
+
+      componentDidMount(){
+        Axios.get('https://panorbit.in/api/users.json')            
+             .then( res => {
+                  
+                 this.setState(
+                     {
+                         
+                         users: res.data.users,
+                     }
+                 )
+             })
+    }
+
+
+  render(){
+           console.log(this.state.users[0]);
   return (
     <div className="App">
       <div >
@@ -11,6 +37,7 @@ function App() {
        </div>
     </div>
   );
+}
 }
 
 export default App;
