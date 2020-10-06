42f33d7c0ad3c39ccc7882e8627eca3d28f7ca87
ajit <ajitkrsingh0786@gmail.com>
2020-10-06 18:45:36 +0530

    8th commit: added-navbar

--- src/components/UserHomePage.js
+++ src/components/UserHomePage.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Container } from 'reactstrap';
  
   function UserHomePage( props ){
       return(
-          <div>
-          <h1>Users Home page</h1>
-          <img src={props.user.profilepicture} alt="user image" />
+          <div  className="home-page">
+              <div className="nav-bar">
+                  <ul className="ul-nav-bar">
+                    <li className="li-nav-bar"> <h4>Profile</h4> </li>
+                    <li className="li-nav-bar"> <h4>Posts</h4>  </li>
+                    <li className="li-nav-bar"> <h4>Gallery</h4></li>
+                    <li className="li-nav-bar">  <h4>ToDo</h4>  </li>                  
+                  </ul>                                      
+              </div>
+              <div  className="home-page-body">
+                  <div className="row-1">
+                       <h4 className="row-1">Users Home page</h4>
+                        <div className="user-image user-row">
+                                   <img src={props.user.profilepicture} alt="img" className="user-image"/>
+                                </div>
+
+                                <div className="user-row">
+                                     {props.user.name}
+                        </div>
+                  </div>
+                  <div className="row-2">
+               
+               <img src={props.user.profilepicture} alt="user image" />
+               </div>
+             </div>
           </div>
       )
   }
